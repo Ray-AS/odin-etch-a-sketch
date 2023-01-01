bf39a3cfc532a6612c34618fbf0259b1dcab72df
Suthan <abisuthans.business@gmail.com>
2023-01-01 11:56:21 -0500

    Add hover effects to grid

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const sketchpad = document.querySelector('.sketchpad');
 
 function generateRandomColor()
 {
-  return Math.floor(Math.random() * 256);
+  return Math.floor(Math.random() * 16777215).toString(16);
 }
 
 for(let i = 0; i < 16 * 16; i++) 
@@ -12,3 +12,10 @@ for(let i = 0; i < 16 * 16; i++)
   sketchpad.appendChild(square);
 }
 
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+  square.addEventListener('mouseover', (div) => {
+    div.currentTarget.style.backgroundColor = "#" + generateRandomColor();
+  });
+});
